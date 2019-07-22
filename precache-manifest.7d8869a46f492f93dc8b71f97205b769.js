fdc830d951ea5d93907647a89241027ebd18b907
kommunistMaou <kommunistmaou@gmail.com>
2019-07-22 12:21:23 +0300

    Updates

---
+++ precache-manifest.7d8869a46f492f93dc8b71f97205b769.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed7d4b3cb9a2b48e47112dfa1bc71d2c",
+    "url": "/phonexa-test/index.html"
+  },
+  {
+    "revision": "d7737700f843a87e37c3",
+    "url": "/phonexa-test/static/css/main.1a3be4ec.chunk.css"
+  },
+  {
+    "revision": "b27da63efdeaf5261bf4",
+    "url": "/phonexa-test/static/js/2.612d496e.chunk.js"
+  },
+  {
+    "revision": "d7737700f843a87e37c3",
+    "url": "/phonexa-test/static/js/main.e367092c.chunk.js"
+  },
+  {
+    "revision": "df072699b5bf1d578522",
+    "url": "/phonexa-test/static/js/runtime~main.832265b8.js"
+  },
+  {
+    "revision": "1bb6787b47b4e66734c9a0cd79a6112f",
+    "url": "/phonexa-test/static/media/background.1bb6787b.jpg"
+  }
+]);
